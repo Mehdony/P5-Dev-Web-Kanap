4921f6dc79c75df782da96810d1090adfac4b268
Mehdony <86969900+Mehdony@users.noreply.github.com>
2021-11-21 13:48:08 +0100

    Affichage des produits de la page d'accueil

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,1 +1,23 @@
+// Appel de l'ID "items" transformé en variable "items"
+let items = document.querySelector("#items")
 
+fetch("http://localhost:3000/api/products")
+.then(response  => response.json())
+.then(products => {
+    let articles = ""
+for (let product of products){
+    articles += displayProducts(product)
+    items.innerHTML = articles
+}
+})
+
+function displayProducts(product) {
+    return `<a href="./product.html?id=${product._id}">
+    <article>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
+    </article>
+  </a>`
+
+}
