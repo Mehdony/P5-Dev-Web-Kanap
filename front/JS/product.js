37011b63d953a6857a5e13d50a51c93059cc0447
Mehdony <86969900+Mehdony@users.noreply.github.com>
2021-11-22 15:22:51 +0100

    Description avec des commentaires

--- front/JS/product.js
+++ front/JS/product.js
@@ -10,25 +10,36 @@ console.log(urlParams)
 const id = urlParams.get('id')
 console.log(id)
 
+// on fait une requete pour obetenir un produit avec un ID
+// On rajoute ${id} pour affficher les id 
 fetch(`http://localhost:3000/api/products/${id}`)
 .then(response  => response.json())
+// on utilise la réponse pour afficher image, titre etc
 .then(product => {
 
+  // L'IMAGE
+  // On récupère le container de l'image et on le stock dans imageContainer
   let imageContainer = document.querySelector('.item__img')
+  // On crée une variable articleImg dans laquelle on stockera l'image
   let articleImg =''
+  // on stock le resultat de la fonction displayImage (l'image produit) dans articleImg
   articleImg += displayImg(product) 
+  // on integre articleImg à imageContainer grace à innerHTML
   imageContainer.innerHTML = articleImg
 
+  // LE TITRE
+  // On récupère la balise title et on y integre du text ( product.name soit le nom du produit dans l'api)
   let title = document.getElementById('title')
   title.innerText = product.name
-
+  
+  // LE PRIX
+  // On récupère la balise price et on y integre du text ( product.price soit le prix du produit dans l'api)
   let price = document.getElementById('price')
   price.innerText = product.price
 
-  
-
 })
 
+// on crée une fonction qui permet de retourner du code html dynamique
 function displayImg(product) {
     return `<img src=${product.imageUrl} alt="Photographie d'un canapé"></img>`
 }
--- front/JS/script.js
+++ front/JS/script.js
@@ -1,17 +1,26 @@
-// Appel de l'ID "items" transformé en variable "items"
+// on va récupérer dans le dom le container 'items' afin de par la suite lui integrer du contenu 
 let items = document.querySelector("#items")
 
+//  On fait un appel à l'API grâce a 'fetch' et ("l'url de l'API") 
 fetch("http://localhost:3000/api/products")
+// ensuite on prend la réponse et on la converti en Json
 .then(response  => response.json())
-.then(products => {
+// ensuite on prend la réponse et on effectue les actions désirées 
+.then(response => {
+    // on crée une variable article vide qui contiendra les produits à afficher
     let articles = ""
-for (let product of products){
+    // parcour chacun des produits de la réponse (qui contient les objets de l'api)
+    for (let product of response){
+    //on ajoute a article le resultat de la fonction
     articles += displayProducts(product)
+    // on integre article à notre container "items"
     items.innerHTML = articles
 }
 })
 
+// on crée une fonction qui permet d'afficher des produits
 function displayProducts(product) {
+    // la fonction retrounera du code html de manière dynamique
     return `<a href="./product.html?id=${product._id}">
     <article>
       <img src="${product.imageUrl}" alt="${product.altTxt}">
