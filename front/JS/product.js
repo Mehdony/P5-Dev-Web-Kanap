1a6cf54d3dd2d16c58ab6b98fc9beff373d3b08f
Mehdony <86969900+Mehdony@users.noreply.github.com>
2021-11-23 15:46:22 +0100

    Mise en place des options et des descriptions

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,17 +1,18 @@
-// permet de stocker l'url qui suit product.html soit ?id=0651616
+//  Permet recuperer les données de l'url de la page (www.google:// id=qslr357446szerteqe  name=qdrmogiphuj   color=qsdrpiugh) 
+// const chainedecaractere = surlapage.lachainederequetedanslurl.recherche
 const queryString = window.location.search
 console.log(queryString)
 
-// permet d'analyser les paramètres de  la chaîne de requête
+// permet d'analyser les paramètres de  la chaîne de requête (parametre dans la console)
 const urlParams = new URLSearchParams(queryString)
 console.log(urlParams)
 
-//permet d'obtenir l'id
+//permet d'obtenir l'id (id de la page (url))
 const id = urlParams.get('id')
 console.log(id)
 
 // on fait une requete pour obetenir un produit avec un ID
-// On rajoute ${id} pour affficher les id 
+// On rajoute ${id} pour afficher les id 
 fetch(`http://localhost:3000/api/products/${id}`)
 .then(response  => response.json())
 // on utilise la réponse pour afficher image, titre etc
@@ -37,10 +38,30 @@ fetch(`http://localhost:3000/api/products/${id}`)
   let price = document.getElementById('price')
   price.innerText = product.price
 
+  // DESCRIPTION
+
+  let description = document.getElementById('description')
+  description.innerText = product.description
+
+  // OPTIONS
+  let selection = document.getElementById('colors')
+  let options = `<option value="">--SVP, choisissez une couleur --</option>`
+  
+  for (let option of product.colors){
+    //on ajoute a article le resultat de la fonction
+    options += displayOptions(option)
+    // on integre article à notre container "items"
+    selection.innerHTML = options
+}
+
 })
+
 
 // on crée une fonction qui permet de retourner du code html dynamique
 function displayImg(product) {
     return `<img src=${product.imageUrl} alt="Photographie d'un canapé"></img>`
+}
+function displayOptions(product) {
+    return ` <option value="${product}">${product}</option>`
 }
 
