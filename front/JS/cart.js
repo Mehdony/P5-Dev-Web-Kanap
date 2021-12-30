a74adf02408c95b5bef374e5ba203b2b8725de7c
Mehdony <86969900+Mehdony@users.noreply.github.com>
2021-12-30 15:40:38 +0100

    suppression des commentaires inutiles

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -9,7 +9,7 @@ displayTotal()
 function displayCart(cart) {
   console.log(cart)
   let modele = ""
-
+  
   for (let product of cart) {
     modele += `<article class="cart__item" data-id=${product.id} data-color="${product.option}">
     <div class="cart__item__img">
@@ -72,7 +72,7 @@ function modifyQtt() {
       event.preventDefault();
 
       //Selection de l'element à modifier en fonction de son id ET sa couleur
-      let quantityModif = cart[k].quantity;
+      const quantityModif = cart[k].quantity;
       let qttModifValue = qttModif[k].valueAsNumber;
 
       const resultFind = cart.find((el) => el.qttModifValue !== quantityModif);
@@ -88,49 +88,3 @@ function modifyQtt() {
   }
 }
 
-// let addNewItem = function(quantity) {
-
-//   // retrieve it (Or create a blank array if there isn't any info saved yet),
-//   let items = JSON.parse(localStorage.getItem("cart"))
-
-//   // add to it, only if it's empty
-//   let item = items.find(item => item.id === id);
-
-//   if (item) {
-//     item.quantity = newvalue;
-//   } else {
-//     items.push({
-//       id,
-//       quantity,
-//       price
-//     })
-//   }
-
-//   // then put it back.
-//   localStorage.setItem('cart', JSON.stringify(items));
-//   console.log(items);
-// }
-// addNewItem(newvalue)
-
-// let produit = {
-//   id : idLS,
-//   qty : qtyLS
-// }
-// console.log(produit);
-
-// const optionItem = document.getElementById('colors').value
-
-//     const cart = JSON.parse(localStorage.getItem('cart')) || []
-//       const founded = cart.find(e => e.id === id && e.option === optionItem)
-
-//       if (founded) {
-//         founded.quantity += newvalue
-//       } else {
-//         cart.push({
-
-//           quantity: newvalue,
-
-//         })
-//       }
-//       localStorage.setItem('cart', JSON.stringify(cart))
-//       window.location.href = 'cart.html'
