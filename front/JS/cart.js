3a9a503d24dcc749cb83006795e5aa40dc85be13
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-01-02 17:25:22 +0100

    Mise en place de la suppression de produits dans le panier

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,15 +1,16 @@
 //  On récupère les données du localstorage grace à la méthode getItem
-const cart = JSON.parse(localStorage.getItem("cart"));
-const cartContainer = document.getElementById("cart__items");
+const cart = JSON.parse(localStorage.getItem("cart"))
+const cartContainer = document.getElementById("cart__items")
 // const qtyValue = document.getElementsByClassName('itemQuantity')
 displayCart(cart)
-modifyQtt()
+updateQuantity()
 displayTotal()
+deleteProduct()
 
 function displayCart(cart) {
   console.log(cart)
   let modele = ""
-  
+
   for (let product of cart) {
     modele += `<article class="cart__item" data-id=${product.id} data-color="${product.option}">
     <div class="cart__item__img">
@@ -34,57 +35,110 @@ function displayCart(cart) {
   </article> `
   }
 
-  cartContainer.innerHTML = modele;
+  cartContainer.innerHTML = modele
   displayTotal()
 }
 
 function displayTotal() {
   const cart = JSON.parse(localStorage.getItem("cart"));
-  let total = 0;
+  let total = 0
   let product = 0
   // Boucle
 
   let totalPriceCart = []
   let totalArticleCart = []
 
-  for (let i = 0; i < cart.length; i++) {
-    let qtyLS = cart[i].quantity
-    let totalProduct = cart[i].price * qtyLS;
+  for (let product of cart) {
+    let quantityLocalStorage = product.quantity
+    let totalProduct = product.price * quantityLocalStorage;
     totalPriceCart.push(totalProduct)
-    totalArticleCart.push(qtyLS)
+    totalArticleCart.push(quantityLocalStorage)
   }
 
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  product = totalArticleCart.reduce(reducer, 0)
-  total = totalPriceCart.reduce(reducer, 0);
-  console.log(total);
 
-  document.getElementById("totalQuantity").innerHTML = product;
-  document.getElementById("totalPrice").innerHTML = total;
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  product = totalArticleCart.reduce(reducer, 0)
+  total = totalPriceCart.reduce(reducer, 0)
+  console.log(total)
+
+  document.getElementById("totalQuantity").innerHTML = product
+  document.getElementById("totalPrice").innerHTML = total
   // Enregristré dans le localstorage
 }
 
-function modifyQtt() {
-  let qttModif = document.querySelectorAll(".itemQuantity");
+function updateQuantity() {
 
-  for (let k = 0; k < qttModif.length; k++) {
-    qttModif[k].addEventListener("change", (event) => {
-      event.preventDefault();
+  let itemQuantity = document.querySelectorAll(".itemQuantity")
 
+  for (let k = 0; k < itemQuantity.length; k++) {
+    itemQuantity[k].addEventListener("change", (event) => {
+
+      event.preventDefault()
       //Selection de l'element à modifier en fonction de son id ET sa couleur
-      const quantityModif = cart[k].quantity;
-      let qttModifValue = qttModif[k].valueAsNumber;
 
-      const resultFind = cart.find((el) => el.qttModifValue !== quantityModif);
+      const baseQuantity = cart[k].quantity
+      let newQuantity = itemQuantity[k].valueAsNumber
 
-      resultFind.quantity = qttModifValue;
-      cart[k].quantity = resultFind.quantity;
+      const resultFind = cart.find((el) => el.newQuantity !== baseQuantity)
 
-      localStorage.setItem("cart", JSON.stringify(cart));
+      resultFind.quantity = newQuantity
+      cart[k].quantity = resultFind.quantity
+      localStorage.setItem("cart", JSON.stringify(cart))
 
       // refresh rapide
-      location.reload();
+      location.reload()
+
     });
+
+  }
+
+}
+
+function deleteProduct() {
+  let btn_supprimer = document.querySelectorAll(".deleteItem");
+
+  for (let i = 0; i < btn_supprimer.length; i++) {
+    btn_supprimer[i].addEventListener("click", (event) => {
+      event.preventDefault()
+
+      //Selection de l'element à supprimer en fonction de son id ET sa couleur
+      let idDelete = cart[i].id
+      let colorDelete = cart[i].option
+      const productToDelete = cart.filter(el => el.id !== idDelete || el.option !== colorDelete)
+
+      localStorage.setItem("cart", JSON.stringify(productToDelete))
+
+      //Alerte produit supprimé et refresh
+      alert("Ce produit a bien été supprimé de votre panier")
+      location.reload()
+    })
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // for (let i = 0; i < cart.length; i++) {
+  //   let qtyLS = cart[i].quantity
+  //   let totalProduct = cart[i].price * qtyLS;
+  //   totalPriceCart.push(totalProduct)
+  //   totalArticleCart.push(qtyLS)
+  // }
